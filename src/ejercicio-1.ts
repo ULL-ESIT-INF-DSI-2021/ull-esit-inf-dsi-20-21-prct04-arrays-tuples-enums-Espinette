cd75d5e9a872a158eb96b84064353cb6efbaec9a
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-03-09 19:05:03 +0000

    Documentacion Ej 1 generada

--- src/ejercicio-1.ts
+++ src/ejercicio-1.ts
@@ -1,4 +1,12 @@
-
+/**
+ * __Funcion para descodificar un resistor en funcion de sus colores__
+ * Negro -> 0; Marrón -> 1; Rojo -> 2; Naranja -> 3; Amarillo -> 4; Verde -> 5; Azul -> 6; Violeta -> 7; Gris -> 8; Blanco -> 9
+ * @param resistor Array de colores (puedes introducir tantos colores como quieras)
+ * @returns Retorna el numero formado por la extración de los 2 primeros colores introducidos
+ * ```TypeScript
+ * decodeResistor(`Marron`,`Verde`) returns 15
+ * ```
+ */
 export function decodeResistor(resistor: string[]) {
   const colores: string[] =[`Negro`, `Marron`, `Rojo`, `Naranja`, `Amarillo`, `Verde`, `Azul`, `Violeta`, `Gris`, `Blanco`];
   let solucion: string = '';
