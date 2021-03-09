a76a7873b8bc6019b4b70f14d8b97bcf0de9d251
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-03-09 17:42:52 +0000

    Expectativa ejercicio 1 creada

--- src/ejercicio-1.ts
+++ src/ejercicio-1.ts
@@ -1,18 +1,7 @@
-/**
- * Adds two numbers
- * @param firstNumber Consists of the first numeric operand of the addition
- * @param secondNumber Consists of the second numeric operand of the addition
- * @return The addition of the two numbers `firstNumber` and `secondNumber`
- *
- * Usage:
- * ```typescript
- * add(1, 7) = 8
- * add(1.7, 3.5) = 5.2
- * ```
- */
-export function add(firstNumber: number, secondNumber: number) {
-  return firstNumber + secondNumber;
+
+export function decodeResistor(colores: string[]) {
+  colores
 }
 
-console.log(`add(1, 7): ${add(1, 7)}`);
-console.log(`add(1.7, 3.5): ${add(1.7, 3.5)}`);
+console.log(`add(1, 7): ${decodeResistor(1, 7)}`);
+console.log(`add(1.7, 3.5): ${decodeResistor(1.7, 3.5)}`);
