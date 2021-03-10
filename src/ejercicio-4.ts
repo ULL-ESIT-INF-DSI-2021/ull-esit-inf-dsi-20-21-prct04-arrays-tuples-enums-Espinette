77b1480b785d5513e50ad1e05609db7ac1a570fe
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-03-10 12:04:21 +0000

    Ejercicio 4 realizado

--- src/ejercicio-4.ts
+++ src/ejercicio-4.ts
@@ -1,3 +1,15 @@
 export function moveZeros(array: number[]) {
-  
+  let aux: number = 0;
+  array.forEach((numero) => {
+    let pos: number = array.indexOf(numero);
+    if (numero == 0) {
+      while (pos + 1 < array.length) {
+        aux = array[pos];
+        array[pos] = array[pos + 1];
+        array[pos + 1] = aux;
+        pos++;
+      }
+    }
+  });
+  return array;
 }
