00affe2382bf9fc92e889db8efc27619bd0315d5
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-03-10 10:37:01 +0000

    Creada expectativa ejercicio 3

---
+++ tests/ejercicio-3.spec.ts
@@ -0,0 +1,13 @@
+import 'mocha';
+import {expect} from 'chai';
+import {meanAndConcatenate} from '../src/ejercicio-3';
+
+describe('Ejercicio 3 - Calcular la media y concatenar cadenas', () => {
+  it('meanAndConcatenate(["u", 6, "d", 1, "i", "w", 6, "s", "t", 4, "a", 6, "g", 1, 2, "w", 8, "o", 2, 0]) returns value [3.6, "udiwstagwo"]', () => {
+    expect(meanAndConcatenate([['u', 6, 'd', 1, 'i', 'w', 6, 's', 't', 4, 'a', 6, 'g', 1, 2, 'w', 8, 'o', 2, 0]])).to.be.equal([3.6, `udiwstagwo`]);
+  });
+
+  it('meanAndConcatenate([`h`, `o`, 5, `l`, 10, 7, `a`, 2]) returns value [8, `hola`]', () => {
+    expect(meanAndConcatenate([`h`, `o`, 5, `l`, 10, 7, `a`, 2])).to.be.equal([8, `hola`]);
+  });
+});
