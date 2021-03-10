f5ab07ecda5c173261e3698e384d6c8016059417
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-03-10 12:09:32 +0000

    Documentacion ejercicio 4 hecha

--- src/ejercicio-4.ts
+++ src/ejercicio-4.ts
@@ -1,3 +1,12 @@
+/**
+ * __Funcion para mover los ceros de un array al final__
+ * @param array Array de numeros
+ * @returns El array con los 0 al final del array
+ * ```TypeScript
+ * moveZeros([1, 0, 1, 2, 0, 1, 3]) -> [1, 1, 2, 1, 3, 0, 0]
+ * ```
+ */
+
 export function moveZeros(array: number[]) {
   let aux: number = 0;
   array.forEach((numero) => {
