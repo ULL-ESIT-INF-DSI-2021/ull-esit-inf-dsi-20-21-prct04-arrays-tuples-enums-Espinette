48685c10a2374149fddca8269e94b7109bc33c99
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-03-10 10:14:18 +0000

    Ejercicio 2 completado test superado

--- src/ejercicio-2.ts
+++ src/ejercicio-2.ts
@@ -1,3 +1,24 @@
 export function meshArray(arrayCadena: string[]) {
-  return arrayCadena;
+  let salida: string = ``;
+  for (let i: number = 0; i < arrayCadena.length - 1; i++) {
+    const word: string = arrayCadena[i];
+    const nextWord: string = arrayCadena[i + 1];
+    let pos: number= word.length - 1;
+    let posNextWord: number = 0;
+    while (word[pos] != nextWord[posNextWord]) {
+      pos--;
+      if (pos < 0) {
+        return `Error al encadenar`;
+      }
+    }
+    for (let j: number = pos; j < word.length; j++) {
+      if (word[j] == nextWord[posNextWord]) {
+        salida = salida + word[j];
+        posNextWord++;
+      } else {
+        return `Error al encadenar`;
+      }
+    }
+  }
+  return salida;
 }
--- tests/ejercicio-2.spec.ts
+++ tests/ejercicio-2.spec.ts
@@ -2,12 +2,12 @@ import 'mocha';
 import {expect} from 'chai';
 import {meshArray} from '../src/ejercicio-2';
 
-describe('Ejercicio 2 - Decodificar resistencias', () => {
+describe('Ejercicio 2 - Palabras encadenadas en un array', () => {
   it('meshArray([“allow”, “lowering”, “ringmaster”, “terror”]) returns value lowringter', () => {
     expect(meshArray([`allow`, `lowering`, `ringmaster`, `terror`])).to.be.equal(`lowringter`);
   });
 
-  it('meshArray([“kingdom”, “dominator”, “notorious”, “usual”, “allegory”]) returns value 15', () => {
+  it('meshArray([“kingdom”, “dominator”, “notorious”, “usual”, “allegory”]) returns value `Error al encadenar`', () => {
     expect(meshArray([`kingdom`, `dominator`, `notorious`, `usual`, `allegory`])).to.be.equal(`Error al encadenar`);
   });
 });
