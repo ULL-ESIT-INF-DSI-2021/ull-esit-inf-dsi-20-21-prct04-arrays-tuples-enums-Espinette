1b9959b19de7ac237f6c15f9c5f0986160552825
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-03-11 18:17:23 +0000

    Documentacion ejercicio 8 generada

--- src/ejercicio-8.ts
+++ src/ejercicio-8.ts
@@ -1,5 +1,15 @@
 type Point = [number, number];
-
+/**
+ * __Funcion para mostrar el recorrido de un punto a otro__
+ * @param x Ancho del tablero
+ * @param y Alto del tablero
+ * @param initialPoint Punto de inicio
+ * @param endPoint Punto de destino
+ * @returns Vector indicando norte, sur, este, oeste segun su desplazamiento
+ * ```TypeScript
+ * agent(10, 10, (1,3),(3,5)) -> [ Este, Norte, Este, Norte ]
+ * ```
+ */
 export function agent(x: number, y: number, initialPoint: Point, endPoint: Point) {
   const recorrido: string[] = [];
   while ((initialPoint[0] != endPoint[0]) && (initialPoint[1] != endPoint[1])) {
