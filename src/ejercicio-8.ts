fc27f3b0fdd0d753e46e4d4d7270a3854bb791b6
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-03-11 18:14:05 +0000

    Ejercicio 8 resuelto

--- src/ejercicio-8.ts
+++ src/ejercicio-8.ts
@@ -1,4 +1,30 @@
 type Point = [number, number];
+
 export function agent(x: number, y: number, initialPoint: Point, endPoint: Point) {
-  
+  const recorrido: string[] = [];
+  while ((initialPoint[0] != endPoint[0]) && (initialPoint[1] != endPoint[1])) {
+    if (initialPoint[0] < endPoint[0]) {
+      recorrido.push(`Este`);
+      initialPoint[0]++;
+    }
+    if (initialPoint[0] > endPoint[0]) {
+      recorrido.push(`Oeste`);
+      initialPoint[0]--;
+    }
+    if (initialPoint[1] < endPoint[1]) {
+      recorrido.push(`Norte`);
+      initialPoint[1]++;
+    }
+    if (initialPoint[1] > endPoint[1]) {
+      recorrido.push(`Sur`);
+      initialPoint[1]--;
+    }
+    console.log(endPoint);
+  }
+  console.log(`hola`);
+  return recorrido;
 }
+
+const initialPoint: Point = [1, 3];
+const endPoint: Point = [3, 5];
+console.log(agent(10, 10, initialPoint, endPoint));
--- tests/ejercicio-8.spec.ts
+++ tests/ejercicio-8.spec.ts
@@ -4,10 +4,10 @@ import {agent} from '../src/ejercicio-8';
 
 
 describe('Ejercicio 8 - El agente', () => {
-  it('agent(10, 10, [1, 3], [3, 5]) returns value [North, North, East, East]', () => {
-    expect(agent(10, 10, [1, 3], [3, 5])).to.be.deep.equal([`North`, `North`, `East`, `East`]);
+  it('agent(10, 10, [1, 3], [3, 5]) returns value [ Este, Norte, Este, Norte ]', () => {
+    expect(agent(10, 10, [1, 3], [3, 5])).to.be.deep.equal([`Este`, `Norte`, `Este`, `Norte`]);
   });
-  it('agent(10, 10, [4, 2], [1, 3]) returns value [North, North, East, East]', () => {
-    expect(agent(10, 10, [4, 2], [1, 3])).to.be.deep.equal([`North`, `North`, `East`, `East`]);
+  it('agent(10, 10, [4, 2], [1, 3]) returns value [Oeste, Norte]', () => {
+    expect(agent(10, 10, [4, 2], [1, 3])).to.be.deep.equal([`Oeste`, `Norte`]);
   });
 });
