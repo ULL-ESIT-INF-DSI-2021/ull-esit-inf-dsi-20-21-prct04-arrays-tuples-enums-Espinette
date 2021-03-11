7a01f3546e241c2aab79138b3db3f49f37de037b
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-03-11 10:42:22 +0000

    Ejercicio 5 resuelto y documentacion generada

--- src/ejercicio-5.ts
+++ src/ejercicio-5.ts
@@ -1,3 +1,14 @@
+/**
+ * __Funcion que lo que hace es multiplicar por un valor cada elemento del vector__
+ * @param arrayNum Array con los numeros a multiplicar
+ * @returns Retorna una llamada a una funcion anónima
+ * ```TypeScript
+ * multiplyAll([2,6,8])(3) -> [6, 18, 24]
+ * ```
+ * __Función anónima__
+ * @param mult Numero por el que multiplicar
+ * @returns Salida de un nuevo array multiplicado
+ */
 
 export function multiplyAll(arrayNum: number[]) {
   return (mult: number) => {
